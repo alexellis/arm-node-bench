e5915ed5f819e66ac4fe8f491d6b19a56222b55c
Alex <alexellis2@gmail.com>
2016-04-15 22:06:07 +0000

    Headlines

--- node_4.x/make.sh
+++ node_4.x/make.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # Uncomment if you do not have this
 #wget http://conoroneill.net.s3.amazonaws.com/wp-content/uploads/2015/07/node-v0.12.7-linux-arm-v7.tar.gz
-docker build -t alexellis2/node_4.xsqlite:armv7 .
+docker build -t alexellis2/node_4.x:armhf .
+
